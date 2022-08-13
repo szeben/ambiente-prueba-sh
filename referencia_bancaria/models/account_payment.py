c17d6c6b42b82d108423b452c73ea6fc62bbb314
fthielen <franthielengaravito@gmail.com>
2022-08-13 12:41:32 -0400

    Cambios correspondientes al modulo Registrar Pagos

--- referencia_bancaria/models/account_payment.py
+++ referencia_bancaria/models/account_payment.py
@@ -6,15 +6,26 @@ from odoo.exceptions import ValidationError
 class accountPaymeRegister(models.TransientModel):
     _inherit = "account.payment.register"
 
-    referencia = fields.Char(string="Nro. Referencia", copy=False, help="Ingrese el Nro. de Referencia del Banco")
+    referencia = fields.Char(string="Nro. Referencia", copy=False, help="Ingrese el Nro. de Referencia del Banco")  
+    is_bank_selected = fields.Boolean(string="is bank selected", default=False)
 
     _sql_constraints = [
         ('referencia_unique', 'unique(referencia)', 'Nro. de Referencia ya existe!')
     ]
 
-    def action_create_payments(self):
+    @api.onchange('journal_id')
+    def _change_journal_id(self):
+        if self.journal_id.type == 'bank':
+            self.is_bank_selected = True
+        else:     
+            self.is_bank_selected = False 
+            self.referencia = ''
+        print(self.is_bank_selected)
 
-        if self.referencia == False and self.journal_id.id == 7:
+
+    def action_create_payments(self):
+        if self.referencia == False and self.journal_id.type == 'bank':
+            self.referencia = ''
             raise ValidationError('El Campo Nro. de Referencia no puede estar Vacio')
         else:
             self._create_payments()
