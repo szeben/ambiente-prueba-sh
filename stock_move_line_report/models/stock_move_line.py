dd12ab52f208a9086e8699e8a491d05f3574b9f1
Wilmer Ibarra <wibarra.backendevs@gmail.com>
2022-08-16 14:23:44 -0400

    corrigiendo ajuste en la condicion cuando tipo de operacion esta en blanco y validando el origen y el destino para ajustes de inventario con location_id.id != 14 para la salida

--- stock_move_line_report/models/stock_move_line.py
+++ stock_move_line_report/models/stock_move_line.py
@@ -41,7 +41,7 @@ class StockMoveLine(models.Model):
     def _compute_salida(self):
         for record in self:
             #   Cantidad de producto actualizada
-            if record.location_id.id == 14 and record.location_dest_id.name == 'Inventory adjustment':
+            if record.location_id.id != 14 and record.location_dest_id.name == 'Inventory adjustment':
                 record.salida = record.qty_done
 
             #Transferencia Interna
