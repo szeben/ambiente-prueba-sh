8418e0e9ecc889fd8de1cef65f6804c21c11268f
Wilmer Ibarra <wibarra.backendevs@gmail.com>
2022-08-19 19:52:33 -0400

    corrigiendo ajuste en la condicion cuando es Inventory adjustment

--- stock_move_line_report/models/stock_move_line.py
+++ stock_move_line_report/models/stock_move_line.py
@@ -66,11 +66,11 @@ class StockMoveLine(models.Model):
             if existencia == 0:
                 record.saldo_existencia = record.qty_done
             elif record.location_id.id == 14 and record.location_id.name == 'Inventory adjustment':
-                record.saldo_existencia = anterior
+                record.saldo_existencia = anterior - record.qty_done
             elif record.picking_code == False and record.location_dest_id.name == 'Inventory adjustment':
-                record.saldo_existencia = anterior
-            elif record.picking_code == "internal":
                 record.saldo_existencia = record.qty_done + anterior
+            elif record.picking_code == "internal":
+                record.saldo_existencia = anterior
             elif record.picking_code == "outgoing":
                 record.saldo_existencia = anterior - record.qty_done
             else:
